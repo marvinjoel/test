f646ce6b64466839289accaeaa198416ea3eb9c3
MarvinAchas <marvinacha@ghlperu.com>
2025-06-30 17:40:00 -0500

    -

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,9 @@
     'version': '15.0.1.0.0',
     'depends': ['product', 'base', 'sale', 'stock'],
     'data': [
+        'security/falabella_security_models.xml',
+        'security/ir.model.access.csv',
         'data/falabella_data.xml',
-        'security/ir.model.access.csv'
     ],
     'installable': True,
     'application': False,
